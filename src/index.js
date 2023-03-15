ffbb0e0da663162768e2bf1d0af25af4f009e862
NicolasBQ <nicolasbquintero@gmail.com>
2023-03-15 10:57:36 -0500

    CRUD API

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ const morgan = require('morgan');
 
 // settings
 app.set('port', 3000);
-
+app.set('json spaces', 2);
 // middleware
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false})) // Get and support data from simple forms
 app.use(express.json()) // Get and suppot JSON data
 
 // routes
-app.get('/', (req,res) => {
-    res.json({ "Title": "Hello World" });
-})
+app.use(require('./routes/index'));
+app.use('/api/movies' ,require('./routes/movies'));
 
 // starting the server 
 app.listen(app.get('port'), () => {
